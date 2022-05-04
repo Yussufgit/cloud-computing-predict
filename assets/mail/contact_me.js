34ab1deb0d71c04524d4401eee73f56f1626abfd
Yussuf Abdiwali Mohamed <47454746+Yussufgit@users.noreply.github.com>
2022-05-04 20:56:17 +0300

    Update contact_me.js
    API endpoint url

--- assets/mail/contact_me.js
+++ assets/mail/contact_me.js
@@ -24,7 +24,7 @@ $(function () {
             var json = {name: name, phone: phone, email: email, message: message}
             $.ajax({
                 // --- CHANGE THIS LINE TO YOUR OWN API GATEWAY  -------- 
-                url: "{Insert your own AWS API Gateway Endpoint URL here}",
+                url: "https://lgsw5xa6fk.execute-api.eu-west-1.amazonaws.com/default/Lambda_Predict",
                 // ------------------------------------------------------  
                 type: "POST",
                 data: JSON.stringify(json),
